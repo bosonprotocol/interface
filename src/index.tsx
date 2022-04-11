e1a539ebc3db7ede37b0319d777a66bc47d4020d
Albert Folch <albertfolch@redeemeum.com>
2022-04-11 12:57:51 +0200

    feat: add eslint-import-sort

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
+import App from "lib/components/app";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "lib/components/app";
+
 import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
--- src/lib/components/app/App.test.tsx
+++ src/lib/components/app/App.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+
 import App from "./index";
 
 test("renders learn react link", () => {
