26e1edd404a20b0beb473223e922447b634d85f9
Albert Folch <albertfolch@redeemeum.com>
2022-04-19 16:12:02 +0200

    [bp185] chore: playwright-report

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ const config: PlaywrightTestConfig = {
     baseURL: "http://localhost:3000/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry"
+    trace: "retain-on-failure"
   },
 
   /* Configure projects for major browsers */
@@ -46,7 +46,8 @@ const config: PlaywrightTestConfig = {
     {
       name: "chromium",
       use: {
-        ...devices["Desktop Chrome"]
+        ...devices["Desktop Chrome"],
+        video: "retain-on-failure"
       }
     },
 
