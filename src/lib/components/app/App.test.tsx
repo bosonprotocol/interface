376a230b982ca2c6a5ee3af10f786df78f89ec10
Albert Folch <albertfolch@redeemeum.com>
2022-04-19 11:49:50 +0200

    [bp185] fix: ci

--- src/lib/components/app/App.test.tsx
+++ src/lib/components/app/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./index";
 
-test("renders learn react link", () => {
+test("renders App and expects Boson dApp to be displayed", () => {
   render(<App />);
   const linkElement = screen.getByText(/Boson dApp/i);
   expect(linkElement).toBeInTheDocument();
