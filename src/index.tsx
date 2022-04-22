855745c81633a4c42c622f1dd072372ab6db1123
Albert Folch <albertfolch@redeemeum.com>
2022-04-22 09:41:26 +0200

    refactor: sort imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import App from "components/app";
 import { BosonRoutes } from "lib/routes";
-import Landing from "pages/Landing";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import CreateOffer from "./pages/CreateOffer";
+import Landing from "./pages/Landing";
 import ManageOffer from "./pages/manage-offer";
 import reportWebVitals from "./reportWebVitals";
 
