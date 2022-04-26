26b5fcc204474853ee2991e86fbb71da99a70c9e
Albert Folch <albertfolch@redeemeum.com>
2022-04-26 09:51:37 +0200

    test(createoffer.test.tsx): add test to test error while calling IpfsMetadata.storeMetadata

---
+++ src/pages/CreateOffer.test.tsx
@@ -0,0 +1,32 @@
+import { act, render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+
+import CreateOffer from "./CreateOffer";
+
+const storeMetadataErrorMsg = "storeMetadataErrorMsg";
+jest.mock("@bosonprotocol/ipfs-storage", () => {
+  class IpfsMetadataMock {
+    storeMetadata() {
+      return;
+    }
+  }
+  IpfsMetadataMock.prototype.storeMetadata = () =>
+    Promise.reject({
+      message: storeMetadataErrorMsg
+    });
+  return {
+    IpfsMetadata: IpfsMetadataMock
+  };
+});
+test("renders CreateOffer and expects an error to be displayed if IpfsMetadata.storeMetadata fails", async () => {
+  render(
+    <MemoryRouter>
+      <CreateOffer />
+    </MemoryRouter>
+  );
+  act(() => {
+    screen.getByText("Submit").click();
+  });
+  const error = await screen.findByText(storeMetadataErrorMsg);
+  expect(error).toBeInTheDocument();
+});
