75d140e3542c4b563dd852241e62d979952088a6
Albert Folch <albertfolch@redeemeum.com>
2022-05-04 17:34:18 +0200

    feat: add React.Suspense

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,25 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { HashRouter, Route, Routes } from "react-router-dom";
 
-import CreateOffer from "./pages/create-offer/CreateOffer";
-import Explore from "./pages/explore/Explore";
-import Landing from "./pages/landing/Landing";
-import ManageOffer from "./pages/manage-offer";
-import OfferDetail from "./pages/offers/OfferDetail";
+// import CreateOffer from "./pages/create-offer/CreateOffer";
+// import Explore from "./pages/explore/Explore";
+// import Landing from "./pages/landing/Landing";
+// import ManageOffer from "./pages/manage-offer";
+// import OfferDetail from "./pages/offers/OfferDetail";
 import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Unable to find the root element");
 
 const queryClient = new QueryClient();
+const CreateOffer = React.lazy(
+  () => import("./pages/create-offer/CreateOffer")
+);
+const Explore = React.lazy(() => import("./pages/explore/Explore"));
+const ManageOffer = React.lazy(() => import("./pages/manage-offer"));
+const OfferDetail = React.lazy(() => import("./pages/offers/OfferDetail"));
+const Landing = React.lazy(() => import("./pages/landing/Landing"));
+const Loading = () => <p>Loading...</p>;
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
@@ -23,12 +31,54 @@ root.render(
       <HashRouter>
         <Routes>
           <Route path="/" element={<App />}>
-            <Route path={BosonRoutes.Root} element={<Landing />} />
-            <Route path={BosonRoutes.Explore} element={<Explore />} />
-            <Route path={BosonRoutes.CreateOffer} element={<CreateOffer />} />
-            <Route path={BosonRoutes.ManageOffers} element={<ManageOffer />} />
-            <Route path={OffersRoutes.Root} element={<Explore />} />
-            <Route path={OffersRoutes.OfferDetail} element={<OfferDetail />} />
+            <Route
+              path={BosonRoutes.Root}
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <Landing />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={BosonRoutes.Explore}
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <Explore />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={BosonRoutes.CreateOffer}
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <CreateOffer />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={BosonRoutes.ManageOffers}
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <ManageOffer />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={OffersRoutes.Root}
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <Explore />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={OffersRoutes.OfferDetail}
+              element={
+                <React.Suspense fallback={<Loading />}>
+                  <OfferDetail />
+                </React.Suspense>
+              }
+            />
             <Route
               path="*"
               element={
