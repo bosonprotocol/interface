4f992491c7dd0c199196322dfe118bed41fc0953
Albert Folch <albertfolch@redeemeum.com>
2022-06-10 09:53:11 +0200

    refactor: remove console.log

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,6 @@ module.exports = override(
   process.env.USE_BABEL_PLUGIN_ISTANBUL &&
     addBabelPlugins("babel-plugin-istanbul"),
   function override(config) {
-    console.log("override");
     let loaders = config.resolve;
     loaders.fallback = {
       util: require.resolve("util/")
