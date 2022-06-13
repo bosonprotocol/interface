710af2c2efaea4c34bd1ee232b9cdaf00f363a6e
Albert Folch <albertfolch@redeemeum.com>
2022-06-13 11:57:58 +0200

    style: fix wallet modal/rainbowkit modal so that they use css variables

--- src/components/WalletConnectionProvider.tsx
+++ src/components/WalletConnectionProvider.tsx
@@ -22,7 +22,6 @@ interface Props {
 export default function WalletConnectionProvider({ children }: Props) {
   const secondaryColor = useCSSVariable("--secondary");
   const accentColor = useCSSVariable("--accent");
-
   const walletConnectionTheme = merge(darkTheme({ borderRadius: "medium" }), {
     colors: {
       accentColor: secondaryColor,
--- src/lib/utils/hooks/useCSSVariable.ts
+++ src/lib/utils/hooks/useCSSVariable.ts
@@ -1,5 +1,14 @@
-export const useCSSVariable = (variableName: string) => {
-  return getComputedStyle(document.documentElement).getPropertyValue(
-    variableName
-  );
+import { useEffect, useState } from "react";
+
+// all css variables can be found in src/components/app/index.tsx
+export const useCSSVariable = (
+  variableName: "--primary" | "--secondary" | "--accent" | "--accentFooter"
+) => {
+  const [value, setValue] = useState<string>();
+  useEffect(() => {
+    setValue(
+      getComputedStyle(document.documentElement).getPropertyValue(variableName)
+    );
+  }, [document.documentElement]);
+  return value;
 };
