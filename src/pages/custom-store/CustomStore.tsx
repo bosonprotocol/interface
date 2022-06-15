65c03ed7522fb9dbcc6e0ebc1c327cc1c2074a26
Albert Folch <albertfolch@redeemeum.com>
2022-06-15 15:05:26 +0200

    feat: custom store: change redirect to iframe

--- src/pages/custom-store/CustomStore.tsx
+++ src/pages/custom-store/CustomStore.tsx
@@ -54,12 +54,19 @@ export default function CustomStore() {
           <meta charset="UTF-8">
           <meta http-equiv="X-UA-Compatible" content="IE=edge">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Boson Protocol</title>
+          <title>${values.storeName || "Boson Protocol"}</title>
       </head>
       <body>
-          <script>
-              window.location.href = '${window.location.origin}/#/?${queryParams}';
-          </script>
+            <style>
+              html, body {
+                margin: 0;
+                height: 100vh;
+              }
+            </style>
+            <iframe
+              src="${window.location.origin}/#/?${queryParams}"
+              width="100%"
+              height="100%"/>
       </body>
       </html>`;
 
