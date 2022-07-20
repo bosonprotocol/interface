336a5caa3de0c19685c55dfd76b2091834888bb0
Maksymilian Kozak <maksymilian.kozak@arianelabs.com>
2022-07-20 15:28:34 +0200

    feat: dispute chat

--- src/pages/chat/components/ChatConversation.tsx
+++ src/pages/chat/components/ChatConversation.tsx
@@ -6,7 +6,6 @@ import SellerID from "../../../components/ui/SellerID";
 import { colors } from "../../../lib/styles/colors";
 import { useBuyerSellerAccounts } from "../../../lib/utils/hooks/useBuyerSellerAccounts";
 import { Thread } from "../types";
-import Dispute from "./Dispute";
 import Message from "./Message";
 
 const Container = styled.div`
@@ -42,6 +41,71 @@ const Conversation = styled.div<{ $alignStart: boolean }>`
     $alignStart ? "flex-start" : "flex-end"};
   flex-grow: 1;
   background-color: ${colors.lightGrey};
+  padding-bottom: 1.875rem;
+`;
+
+const TypeMessage = styled.div`
+  height: 5rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: 1rem;
+  background: ${colors.lightGrey};
+  border: 0px solid ${colors.border};
+  height: 2.7rem;
+  font-family: "Plus Jakarta Sans";
+  font-style: normal;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.5rem;
+  padding: 0.75rem 1rem 0.75rem 1rem;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const BtnProposal = styled.button`
+  border: 3px solid ${colors.secondary};
+  padding-left: 1.2rem;
+  padding-right: 2.5rem;
+  font-size: 0.875rem;
+  margin-right: 0.875rem;
+  height: 2.7rem;
+  font-weight: 600;
+  color: ${colors.secondary};
+  background-color: transparent;
+  position: relative;
+  span {
+    display: block;
+    height: 0.6em;
+    width: 0.6em;
+    font-size: 2.063rem;
+    text-align: center;
+    line-height: 0.5em;
+    margin: 0;
+    padding: 0;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    font-family: Frutiger, "Frutiger Linotype", Univers, Calibri, "Gill Sans",
+      "Gill Sans MT", "Myriad Pro", Myriad, "DejaVu Sans Condensed",
+      "Liberation Sans", "Nimbus Sans L", Tahoma, Geneva, "Helvetica Neue",
+      Helvetica, Arial, sans-serif;
+    font-weight: 100;
+    color: ${colors.secondary};
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 6px;
+    margin-top: -1px;
+    &:before {
+      display: block;
+      content: "+";
+    }
+  }
 `;
 
 const getWasItSentByMe = (
@@ -93,28 +157,36 @@ export default function ChatConversation({ thread }: Props) {
   }
 
   return (
-    <>
-      <Container>
-        <Header>
-          <SellerComponent size={24} />
-        </Header>
-        <Messages>
-          {thread.messages.map((message) => {
-            return (
-              <Conversation
-                key={message.id}
-                $alignStart={!getWasItSentByMe(buyerId, sellerId, message.from)}
+    <Container>
+      <Header>
+        <SellerComponent size={24} />
+      </Header>
+      <Messages>
+        {thread.messages.map((message) => {
+          return (
+            <Conversation
+              key={message.id}
+              $alignStart={!getWasItSentByMe(buyerId, sellerId, message.from)}
+            >
+              <Message
+                thread={thread}
+                message={message}
+                isLeftAligned={
+                  !getWasItSentByMe(buyerId, sellerId, message.from)
+                }
               >
-                <Message thread={thread} message={message}>
-                  <SellerComponent size={32} withProfileText={false} />
-                </Message>
-              </Conversation>
-            );
-          })}
-        </Messages>
-      </Container>
-      {/* {thread.exchange?.disputed && <Dispute thread={thread} />} */}
-      <Dispute thread={thread} />
-    </>
+                <SellerComponent size={32} withProfileText={false} />
+              </Message>
+            </Conversation>
+          );
+        })}
+      </Messages>
+      <TypeMessage>
+        <BtnProposal>
+          Proposal <span>&nbsp;</span>
+        </BtnProposal>
+        <Input placeholder="Write a message" />
+      </TypeMessage>
+    </Container>
   );
 }
--- src/pages/chat/components/Message.tsx
+++ src/pages/chat/components/Message.tsx
@@ -1,43 +1,103 @@
+import { Image as AccountImage } from "@davatar/react";
 import { ReactNode } from "react";
 import styled from "styled-components";
 
 import { colors } from "../../../lib/styles/colors";
 import { Thread } from "../types";
 
-const Content = styled.div`
+const Content = styled.div<{ $isLeftAligned: boolean }>`
   position: relative;
-  background-color: ${colors.white};
+  background-color: ${({ $isLeftAligned }) =>
+    $isLeftAligned ? colors.black : colors.white};
+  color: ${({ $isLeftAligned }) =>
+    $isLeftAligned ? colors.white : colors.black};
   display: flex;
   flex-direction: column;
   padding: 1rem;
-  border: 1px solid #5560720f;
+  border: 0.063rem solid #5560720f;
   margin-right: 2.5rem;
-  margin-top: 4.5rem;
   margin-left: 2.5rem;
+  padding-top: 1.5rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  padding-bottom: 3.75rem;
+  max-width: 31.625rem;
+  margin-top: 3rem;
   &:after {
     position: absolute;
     content: "";
-    border-top: 16px solid ${colors.white};
+    border-top: 16px solid
+      ${({ $isLeftAligned }) => ($isLeftAligned ? colors.black : colors.white)};
     border-right: 16px solid transparent;
-    right: -1rem;
-    top: 0px;
+    border-right: ${({ $isLeftAligned }) =>
+      $isLeftAligned ? "none" : "16px solid transparent"};
+    border-left: ${({ $isLeftAligned }) =>
+      $isLeftAligned ? "16px solid transparent" : "none"};
+    top: -0.063rem;
+    right: ${({ $isLeftAligned }) => ($isLeftAligned ? "auto" : "-1rem")};
+    left: ${({ $isLeftAligned }) => ($isLeftAligned ? "-1rem" : "auto")};
   }
   h4 {
     font-weight: 600;
+    font-size: 1.25rem;
   }
+  h5 {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+  ul {
+    list-style: none;
+    padding: 0rem;
+    margin: 0 0 1.5rem 0;
+    li {
+      &:before {
+        content: "✓";
+        margin-right: 0.125rem;
+      }
+    }
+  }
+`;
+
+const Avatar = styled.div`
+  position: absolute;
+  top: -1.25rem;
+  left: 1rem;
+`;
+
+const Date = styled.div<{ $isLeftAligned: boolean }>`
+  position: absolute;
+  bottom: 1rem;
+  right: ${({ $isLeftAligned }) => ($isLeftAligned ? "auto" : "1rem")};
+  left: ${({ $isLeftAligned }) => ($isLeftAligned ? "1rem" : "auto")};
+  font-size: 0.75rem;
+  color: ${({ $isLeftAligned }) =>
+    $isLeftAligned ? colors.lightGrey : colors.darkGrey};
 `;
 
 interface Props {
   thread: Thread;
   message: Thread["messages"][number];
   children: ReactNode;
+  isLeftAligned: boolean;
 }
 
-export default function Message({ message, children }: Props) {
+export default function Message({
+  message,
+  children,
+  isLeftAligned,
+  thread
+}: Props) {
+  console.log(message);
   if (typeof message.content.value === "string") {
     return (
-      <Content>
-        {children}
+      <Content $isLeftAligned={isLeftAligned}>
+        {isLeftAligned ? (
+          <Avatar>{children}</Avatar>
+        ) : (
+          <Avatar>
+            <AccountImage size={32} address={thread.exchange?.buyer.wallet} />
+          </Avatar>
+        )}
         <h4>Balint raised a dispute and made a proposal</h4>
         <h5>Dispute Category</h5>
         <ul>
@@ -46,13 +106,21 @@ export default function Message({ message, children }: Props) {
             The item received is a different colour, model, version, or size
           </li>
         </ul>
+        <h5>Additional information</h5>
+        <p>
+          Hello there, the item I received has some quality issues. The colours
+          are a bit worn out and not as bright as on the picture. The laces are
+          slightly damaged and in the wrong colour....
+        </p>
         <div>{message.content.value}</div>
-        <button>button</button>
+        <Date $isLeftAligned={isLeftAligned}>
+          {message.sentDate.getHours()}:{message.sentDate.getMinutes()}
+        </Date>
       </Content>
     );
   }
   return (
-    <Content>
+    <Content $isLeftAligned={isLeftAligned}>
       {children}
       <div>{JSON.stringify(message.content.value)}</div>
     </Content>
