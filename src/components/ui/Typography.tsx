23a11a0750ae893f1afaf5195e12ab53cac45d27
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2022-07-22 10:23:01 +0200

    feat: change Grid and Typography props (#82)
    * feat: add more props to Grid and Typography

--- src/components/ui/Typography.tsx
+++ src/components/ui/Typography.tsx
@@ -1,3 +1,39 @@
+import styled from "styled-components";
+
+import { IGrid } from "./Grid";
+
+interface WrapperProps extends IGrid {
+  fontSize?: string;
+  fontWeight?: string;
+  color?: string;
+  background?: string;
+  cursor?: string;
+}
+
+const Wrapper = styled.div<WrapperProps>`
+  display: flex;
+  ${({ alignItems }) => alignItems && `align-items: ${alignItems}`};
+  ${({ flexBasis }) => flexBasis && `flex-basis: ${flexBasis}`};
+  ${({ flexDirection }) => flexDirection && `flex-direction: ${flexDirection}`};
+  ${({ flexGrow }) => flexGrow && `flex-grow: ${flexGrow}`};
+  ${({ justifyContent }) =>
+    justifyContent && `justify-content: ${justifyContent}`};
+  ${({ flexWrap }) => (flexWrap ? `flex-wrap:${flexWrap};` : "")}
+  ${({ rowGap }) => (rowGap ? `row-gap:${rowGap};` : "")}
+  ${({ columnGap }) => (columnGap ? `column-gap:${columnGap};` : "")}
+  ${({ gap }) => (gap ? `gap:${gap};` : "")}
+  ${({ flex }) => (flex ? `> * { flex: ${flex}; }` : "")}
+  ${({ padding }) => (padding ? `padding:${padding};` : "")}
+  ${({ margin }) => (margin ? `margin:${margin};` : "")}
+
+
+  ${({ fontSize }) => (fontSize ? `font-size:${fontSize};` : "")}
+  ${({ fontWeight }) => (fontWeight ? `font-weight:${fontWeight};` : "")}
+  ${({ color }) => (color ? `color:${color};` : "")}
+  ${({ background }) => (background ? `background:${background};` : "")}
+  ${({ cursor }) => (cursor ? `cursor:${cursor};` : "")}
+`;
+
 interface ITypography {
   children?: string | React.ReactNode;
   tag?: keyof JSX.IntrinsicElements;
@@ -6,13 +42,13 @@ interface ITypography {
 }
 
 const Typography: React.FC<ITypography> = ({
-  tag: Wrapper = "div",
+  tag = "div",
   children,
   style = {},
   ...props
 }) => {
   return (
-    <Wrapper style={style} {...props}>
+    <Wrapper style={style} {...props} as={tag}>
       {children}
     </Wrapper>
   );
