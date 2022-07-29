940b2c28913f9551ad3de38c5e198e2074fdc520
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-07-29 15:29:41 +0200

    fix: use proper ipfs storage url (#90)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -24,7 +24,7 @@ export const CONFIG = {
   widgetsUrl: process.env.REACT_APP_WIDGETS_URL || config.widgetsUrl,
   chainId: REACT_APP_CHAIN_ID,
   ipfsMetadataUrl:
-    process.env.REACT_APP_IPFS_METADATA_URL || "https://ipfs.infura.io:5001",
+    process.env.REACT_APP_IPFS_METADATA_URL || config.ipfsMetadataUrl,
   sentryDSNUrl:
     "https://ff9c04ed823a4658bc5de78945961937@o992661.ingest.sentry.io/6455090",
   metaTransactionsApiKey: process.env.REACT_APP_META_TX_API_KEY,
@@ -39,22 +39,6 @@ export const CONFIG = {
   )
 };
 
-export const coreSdkConfig: Config = {
-  ...config,
-  chainId: REACT_APP_CHAIN_ID,
-  ipfsMetadataUrl:
-    process.env.REACT_APP_IPFS_METADATA_URL || "https://ipfs.infura.io:5001",
-  protocolDiamond: config.contracts.protocolDiamond
-};
-
-export type Config = {
-  chainId: number;
-  protocolDiamond: string;
-  subgraphUrl: string;
-  jsonRpcUrl: string;
-  theGraphIpfsUrl?: string;
-  ipfsMetadataUrl: string;
-};
 function stringToBoolean(value?: string) {
   if (typeof value === "string") {
     return ["1", "true"].includes(value);
