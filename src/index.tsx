fb95ce2d5ba628bb244577c8752ad9276bc9c3fc
Roberto Sousa <beto.sousa22@gmail.com>
2022-08-10 18:05:15 +0100

    refactor: remove comment

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,6 @@ import WalletConnectionProvider from "./components/WalletConnectionProvider";
 import { BosonRoutes, OffersRoutes } from "./lib/routing/routes";
 import PrivateAccount from "./pages/account/private/PrivateAccountContainer";
 import PublicOrPrivateAccount from "./pages/account/public/PublicOrPrivateAccount";
-// import CreateOffer from "./pages/create-offer/CreateOffer";
 import CreateProduct from "./pages/create-product/CreateProduct";
 import CustomStore from "./pages/custom-store/CustomStore";
 import Exchange from "./pages/exchange/Exchange";
