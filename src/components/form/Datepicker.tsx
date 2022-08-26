36d492494d74e084f047cb4c3a1f605f6a722012
Roberto Sousa <beto.sousa22@gmail.com>
2022-08-26 16:11:47 +0100

    fix: :tada: validate redemption period

--- src/components/form/Datepicker.tsx
+++ src/components/form/Datepicker.tsx
@@ -1,4 +1,5 @@
 import type { Dayjs } from "dayjs";
+import dayjs from "dayjs";
 import { useField } from "formik";
 import React from "react";
 
@@ -9,11 +10,16 @@ import type { DatepickerProps } from "./types";
 export default function DatepickerComponent({
   name,
   period = false,
+  setIsFormValid,
   ...rest
 }: DatepickerProps) {
   const [field, meta, helpers] = useField(name);
-  const errorMessage = meta.error && meta.touched ? meta.error : "";
-  const displayError =
+  const [offerValidityPeriodFields] = useField(
+    "coreTermsOfSale.offerValidityPeriod"
+  );
+
+  let errorMessage = meta.error && meta.touched ? meta.error : "";
+  let displayError =
     typeof errorMessage === typeof "string" && errorMessage !== "";
 
   const handleChange = (date: Dayjs | Array<Dayjs | null>) => {
@@ -22,6 +28,26 @@ export default function DatepickerComponent({
     }
     helpers.setValue(date);
   };
+
+  if (
+    field.name === "coreTermsOfSale.redemptionPeriod" &&
+    period &&
+    setIsFormValid
+  ) {
+    const offerValidityPeriodLastDate = dayjs(
+      offerValidityPeriodFields.value[1]
+    );
+    const redemptionPeriodLastDate = dayjs(field.value[1]);
+    const isNotValid = redemptionPeriodLastDate.isBefore(
+      offerValidityPeriodLastDate
+    );
+    setIsFormValid(!isNotValid);
+    if (isNotValid) {
+      errorMessage =
+        "Redemption period has to be after or equal to validity period";
+      displayError = true;
+    }
+  }
 
   return (
     <>
