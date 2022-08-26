745c2c2825ef3430363c6e6aaeb83d9d8005554a
Albert Folch <albertfolch@redeemeum.com>
2022-08-26 15:53:24 +0200

    fix: supported networks

--- src/lib/wallet-connection.ts
+++ src/lib/wallet-connection.ts
@@ -8,7 +8,8 @@ import { CONFIG } from "./config";
 const supportedChains: Readonly<Array<number>> = [
   chain.ropsten.id,
   chain.mainnet.id,
-  chain.polygonMumbai.id
+  chain.polygonMumbai.id,
+  chain.polygon.id
 ] as const;
 
 function getBosonTestNetworkChainConfig(): Chain {
