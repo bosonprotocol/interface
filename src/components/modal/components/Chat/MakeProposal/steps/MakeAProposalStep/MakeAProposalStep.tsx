8d803d7da365301c0997f35871ecad61486380f5
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2022-09-02 10:18:00 +0200

    feat: fix copy make a proposal (seller/buyer) & fix days left to resolve dispute (#180)

--- src/components/modal/components/Chat/MakeProposal/steps/MakeAProposalStep/MakeAProposalStep.tsx
+++ src/components/modal/components/Chat/MakeProposal/steps/MakeAProposalStep/MakeAProposalStep.tsx
@@ -1,8 +1,10 @@
 import { useField, useFormikContext } from "formik";
 import { useEffect } from "react";
 import styled from "styled-components";
+import { useAccount } from "wagmi";
 
 import { colors } from "../../../../../../../lib/styles/colors";
+import { useBuyers } from "../../../../../../../lib/utils/hooks/useBuyers";
 import { Exchange } from "../../../../../../../lib/utils/hooks/useExchanges";
 import { Select } from "../../../../../../form";
 import Button from "../../../../../../ui/Button";
@@ -33,6 +35,13 @@ export default function MakeAProposalStep({
   onBackClick,
   isValid
 }: Props) {
+  const { address } = useAccount();
+  const { data: buyers = [] } = useBuyers({
+    wallet: address
+  });
+  const myBuyerId = buyers[0]?.id;
+  const iAmTheBuyer = myBuyerId === exchange?.buyer.id;
+  const counterPartyText = iAmTheBuyer ? "seller" : "buyer";
   const [proposalTypeField] = useField<typeof proposals[0]>(
     FormModel.formFields.proposalType.name
   );
@@ -52,9 +61,10 @@ export default function MakeAProposalStep({
         Make a proposal
       </Typography>
       <Typography $fontSize="1.25rem" color={colors.darkGrey}>
-        Here you can make a proposal to the seller on how you would like the
-        issue to be resolved. Note that this proposal is binding and if the
-        seller agrees to it, the proposal will be implemented automatically.
+        Here you can make a proposal to the {counterPartyText} on how you would
+        like the issue to be resolved. Note that this proposal is binding and if
+        the &nbsp;{counterPartyText} agrees to it, the proposal will be
+        implemented automatically.
       </Typography>
       <Grid flexDirection="column" margin="2rem 0 0 0" alignItems="flex-start">
         <Typography fontWeight="600" tag="p" $fontSize="1.5rem">
