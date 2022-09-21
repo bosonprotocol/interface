be6ca5cabeceedc98c29510554a2038172c08d89
Roberto Sousa <beto.sousa22@gmail.com>
2022-09-21 23:07:59 +0100

    feat: mock pages

---
+++ src/components/disputeResolver/pages/DisputeWhiteList.tsx
@@ -0,0 +1,9 @@
+import { DisputeResolverProps } from "../DisputeResolverInside";
+
+export const DisputeWhiteList: React.FC<DisputeResolverProps> = () => {
+  return (
+    <>
+      <h1>Under construction</h1>
+    </>
+  );
+};
---
+++ src/components/disputeResolver/pages/DisputesManage.tsx
@@ -0,0 +1,9 @@
+import { DisputeResolverProps } from "../DisputeResolverInside";
+
+export const DisputesManage: React.FC<DisputeResolverProps> = () => {
+  return (
+    <>
+      <h1>Manage Disputes</h1>
+    </>
+  );
+};
