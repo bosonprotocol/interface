49c95f028c8dc4912cc9b91f3dcc00fa51c799ae
Roberto Sousa <beto.sousa22@gmail.com>
2022-09-27 03:28:00 +0100

    fix: dispute resolver link

--- src/components/header/HeaderLinks.tsx
+++ src/components/header/HeaderLinks.tsx
@@ -7,6 +7,7 @@ import { UrlParameters } from "../../lib/routing/parameters";
 import { BosonRoutes, SellerCenterRoutes } from "../../lib/routing/routes";
 import { colors } from "../../lib/styles/colors";
 import { useBuyerSellerAccounts } from "../../lib/utils/hooks/useBuyerSellerAccounts";
+import { useCurrentDisputeResolverId } from "../../lib/utils/hooks/useCurrentDisputeResolverId";
 import { useCustomStoreQueryParameter } from "../../pages/custom-store/useCustomStoreQueryParameter";
 import { UserRoles } from "../../router/routes";
 import useUserRoles, { checkIfUserHaveRole } from "../../router/useUserRoles";
@@ -147,7 +148,8 @@ export default function HeaderLinks({
   } = useBuyerSellerAccounts(address || "");
   const isAccountSeller = useMemo(() => !!sellerId, [sellerId]);
   const isAccountBuyer = useMemo(() => !!buyerId, [buyerId]);
-
+  // const isDisputeResolver = useMemo(() => !!buyerId, [buyerId]);
+  const { disputeResolverId } = useCurrentDisputeResolverId();
   const sellUrl = useMemo(
     () =>
       isAccountSeller
@@ -157,15 +159,27 @@ export default function HeaderLinks({
         : SellerCenterRoutes.CreateProduct,
     [isAccountSeller]
   );
+
   const isSupportFunctionalityDefined = supportFunctionality !== "";
+  console.log(
+    "🚀  roberto --  ~ file: HeaderLinks.tsx ~ line 161 ~ isSupportFunctionalityDefined",
+    isSupportFunctionalityDefined
+  );
+
   const onlyBuyer =
     typeof supportFunctionality != "string" &&
     supportFunctionality?.length === 1 &&
     supportFunctionality?.[0] === "buyer";
+
   const onlySeller =
     typeof supportFunctionality != "string" &&
     supportFunctionality?.length === 1 &&
     supportFunctionality?.[0] === "seller";
+
+  console.log(
+    "🚀  roberto --  ~ file: HeaderLinks.tsx ~ line 173 ~ supportFunctionality",
+    supportFunctionality
+  );
   return (
     <NavigationLinks
       isMobile={isMobile}
@@ -194,6 +208,12 @@ export default function HeaderLinks({
           address &&
           checkIfUserHaveRole(roles, [UserRoles.Buyer], false) && (
             <LinkWithQuery to={BosonRoutes.YourAccount}>My Items</LinkWithQuery>
+          )}
+        {!!disputeResolverId &&
+          checkIfUserHaveRole(roles, [UserRoles.DisputeResolver], true) && (
+            <LinkWithQuery to={BosonRoutes.DRAdmin}>
+              Dispute Centre
+            </LinkWithQuery>
           )}
       </Links>
     </NavigationLinks>
