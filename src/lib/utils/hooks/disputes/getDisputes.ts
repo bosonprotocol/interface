21c886cd96477141e6858c86603379f113d6ca5c
Roberto Sousa <beto.sousa22@gmail.com>
2022-09-27 01:23:12 +0100

    feat: get disputes

---
+++ src/lib/utils/hooks/disputes/getDisputes.ts
@@ -0,0 +1,59 @@
+import { gql } from "graphql-request";
+import { useQuery } from "react-query";
+import { useAccount } from "wagmi";
+
+import { fetchSubgraph } from "../../core-components/subgraph";
+
+export function ActiveEscalatedDisputes() {
+  const { address: admin } = useAccount();
+  const props = { admin };
+
+  const result = useQuery(["getActiveEscalatedDisputes", props], async () => {
+    const result = await fetchSubgraph<{
+      disputes: {
+        id: string;
+      }[];
+    }>(
+      gql`
+        query GetActiveEscalatedDisputes {
+          disputes(
+            where: {
+              escalatedDate_not: null
+              retractedDate: null
+              exchange_: { completedDate: null }
+            }
+          ) {
+            id
+            state
+            escalatedDate
+            exchange {
+              offer {
+                price
+                exchangeToken {
+                  address
+                  symbol
+                }
+                metadata {
+                  ... on ProductV1MetadataEntity {
+                    name
+                    image
+                  }
+                }
+                disputeResolver {
+                  escalationResponsePeriod
+                }
+              }
+            }
+          }
+        }
+      `,
+      props
+    );
+    return result;
+  });
+
+  return {
+    ...result,
+    data: result?.data?.disputes ?? []
+  };
+}
