b392bf7d2487b9daee12898dcecd10ba31ce1443
RobertoMSousa <beto.sousa22@gmail.com>
2022-09-27 15:53:19 +0100

    refactor: hide resolved button if status is not resolving (#241)

---
+++ src/components/seller/exchanges/SellerAction.tsx
@@ -0,0 +1,138 @@
+import { exchanges as ExchangesKit, subgraph } from "@bosonprotocol/react-kit";
+import { Chat } from "phosphor-react";
+import { generatePath } from "react-router-dom";
+import { NavigateOptions, Path } from "react-router-dom";
+
+import { UrlParameters } from "../../../lib/routing/parameters";
+import { BosonRoutes } from "../../../lib/routing/routes";
+import { useDisputeSubStatusInfo } from "../../../lib/utils/hooks/useDisputeSubStatusInfo";
+import { Exchange } from "../../../lib/utils/hooks/useExchanges";
+import { useModal } from "../../modal/useModal";
+import Button from "../../ui/Button";
+import Grid from "../../ui/Grid";
+import { isCompletable } from "./SellerExchangeTable";
+
+const generatePathAndNavigate = ({
+  exchangeId,
+  navigate
+}: {
+  exchangeId: string;
+  navigate: (to: Partial<Path>, options?: NavigateOptions | undefined) => void;
+}) => {
+  const pathname = generatePath(BosonRoutes.ChatMessage, {
+    [UrlParameters.exchangeId]: exchangeId ?? 0
+  });
+  navigate({ pathname });
+};
+
+export const SellerResolveDisputeButton = ({
+  exchange,
+  navigate
+}: {
+  exchange: Exchange | null;
+  navigate: (to: Partial<Path>, options?: NavigateOptions | undefined) => void;
+}) => {
+  const { status } = useDisputeSubStatusInfo(exchange);
+  if (!exchange || status !== "Resolving") {
+    return (
+      <Button
+        theme="ghostSecondary"
+        size="small"
+        onClick={() => {
+          if (exchange?.id) {
+            generatePathAndNavigate({ exchangeId: exchange?.id, navigate });
+          }
+        }}
+      >
+        Chat <Chat size={14} />
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      theme="primary"
+      size="small"
+      onClick={() => {
+        if (exchange?.id) {
+          generatePathAndNavigate({ exchangeId: exchange?.id, navigate });
+        }
+      }}
+    >
+      Resolve dispute
+    </Button>
+  );
+};
+
+export const SellerActionButton = ({
+  exchange,
+  refetch,
+  navigate,
+  status
+}: {
+  exchange: Exchange | null;
+  refetch: () => void;
+  navigate: (to: Partial<Path>, options?: NavigateOptions | undefined) => void;
+  status: ExchangesKit.AllExchangeStates | string;
+}) => {
+  const { showModal, modalTypes } = useModal();
+  if (!exchange) {
+    return null;
+  }
+  return (
+    <Grid justifyContent="flex-end" gap="1rem">
+      {exchange && isCompletable(exchange) && (
+        <Button
+          theme="bosonPrimary"
+          size="small"
+          onClick={() => {
+            showModal(
+              modalTypes.COMPLETE_EXCHANGE,
+              {
+                title: "Complete Confirmation",
+                exchange: exchange,
+                refetch
+              },
+              "xs"
+            );
+          }}
+        >
+          Complete exchange
+        </Button>
+      )}
+      <Button
+        theme="ghostSecondary"
+        size="small"
+        onClick={() => {
+          if (exchange?.id) {
+            generatePathAndNavigate({ exchangeId: exchange?.id, navigate });
+          }
+        }}
+      >
+        Chat <Chat size={14} />
+      </Button>
+      {status === subgraph.ExchangeState.Committed && (
+        <Button
+          theme="orangeInverse"
+          size="small"
+          onClick={() => {
+            if (exchange) {
+              showModal(
+                modalTypes.REVOKE_PRODUCT,
+                {
+                  title: "Revoke rNFT",
+                  exchangeId: exchange?.id,
+                  exchange: exchange,
+                  refetch
+                },
+                "xs"
+              );
+            }
+          }}
+        >
+          Revoke
+        </Button>
+      )}
+    </Grid>
+  );
+};
