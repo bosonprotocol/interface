bac82214107272aaa3ccdb26fe8186a32255d59d
Roberto Sousa <beto.sousa22@gmail.com>
2022-09-29 23:15:24 +0100

    refactor: simplify the queries

--- src/lib/utils/hooks/disputes/getDisputes.ts
+++ src/lib/utils/hooks/disputes/getDisputes.ts
@@ -5,6 +5,62 @@ import { useAccount } from "wagmi";
 import { fetchSubgraph } from "../../core-components/subgraph";
 import { Disputes } from "../useExchanges";
 
+const buildQuery = (queryString: string, name: string) => {
+  return gql`
+    query ${name} {
+      disputes(
+        where: {
+          ${queryString}
+        }
+      ) {
+        id
+        state
+        escalatedDate
+        retractedDate
+        exchange {
+          id
+          completedDate
+          offer {
+            resolutionPeriodDuration
+            price
+            exchanges {
+              id
+              disputed
+              cancelledDate
+              committedDate
+              completedDate
+              disputedDate
+              expired
+              finalizedDate
+              redeemedDate
+              revokedDate
+              validUntilDate
+              state
+              buyer {
+                id
+              }
+            }
+            exchangeToken {
+              address
+              symbol
+              decimals
+            }
+            metadata {
+              ... on ProductV1MetadataEntity {
+                name
+                image
+              }
+            }
+            disputeResolver {
+              escalationResponsePeriod
+            }
+          }
+        }
+      }
+    }
+  `;
+};
+
 export const GetActiveEscalatedDisputes = () => {
   const { address: admin } = useAccount();
   const props = { admin };
@@ -13,58 +69,13 @@ export const GetActiveEscalatedDisputes = () => {
     const result = await fetchSubgraph<{
       disputes: Disputes[];
     }>(
-      gql`
-        query GetActiveEscalatedDisputes {
-          disputes(
-            where: {
-              escalatedDate_not: null
-              retractedDate: null
-              exchange_: { completedDate: null }
-              state: "ESCALATED"
-            }
-          ) {
-            id
-            state
-            escalatedDate
-            retractedDate
-            exchange {
-              id
-              completedDate
-              offer {
-                resolutionPeriodDuration
-                price
-                exchanges {
-                  disputed
-                  cancelledDate
-                  committedDate
-                  completedDate
-                  disputedDate
-                  expired
-                  finalizedDate
-                  redeemedDate
-                  revokedDate
-                  validUntilDate
-                  state
-                }
-                exchangeToken {
-                  address
-                  symbol
-                  decimals
-                }
-                metadata {
-                  ... on ProductV1MetadataEntity {
-                    name
-                    image
-                  }
-                }
-                disputeResolver {
-                  escalationResponsePeriod
-                }
-              }
-            }
-          }
-        }
-      `,
+      buildQuery(
+        `escalatedDate_not: null
+         retractedDate: null
+         exchange_: { completedDate: null }
+         state: "ESCALATED"`,
+        `getActiveEscalatedDisputes`
+      ),
       props
     );
     return result;
@@ -86,53 +97,10 @@ export const GetPastEscalatedDisputesWithDecisions = () => {
       const result = await fetchSubgraph<{
         disputes: Disputes[];
       }>(
-        gql`
-          query getPastEscalatedDisputesWithDecisions {
-            disputes(
-              where: { escalatedDate_not: null, decidedDate_not: null }
-            ) {
-              id
-              state
-              escalatedDate
-              retractedDate
-              exchange {
-                id
-                completedDate
-                offer {
-                  resolutionPeriodDuration
-                  price
-                  exchanges {
-                    disputed
-                    cancelledDate
-                    committedDate
-                    completedDate
-                    disputedDate
-                    expired
-                    finalizedDate
-                    redeemedDate
-                    revokedDate
-                    validUntilDate
-                    state
-                  }
-                  exchangeToken {
-                    address
-                    symbol
-                    decimals
-                  }
-                  metadata {
-                    ... on ProductV1MetadataEntity {
-                      name
-                      image
-                    }
-                  }
-                  disputeResolver {
-                    escalationResponsePeriod
-                  }
-                }
-              }
-            }
-          }
-        `,
+        buildQuery(
+          `escalatedDate_not: null, decidedDate_not: null`,
+          `getPastEscalatedDisputesWithDecisions`
+        ),
         props
       );
       return result;
@@ -155,53 +123,10 @@ export const GetPastEscalatedDisputesWithRefusals = () => {
       const result = await fetchSubgraph<{
         disputes: Disputes[];
       }>(
-        gql`
-          query getPastEscalatedDisputesWithRefusals {
-            disputes(
-              where: { escalatedDate_not: null, refusedDate_not: null }
-            ) {
-              id
-              state
-              escalatedDate
-              retractedDate
-              exchange {
-                id
-                completedDate
-                offer {
-                  resolutionPeriodDuration
-                  price
-                  exchangeToken {
-                    address
-                    symbol
-                    decimals
-                  }
-                  exchanges {
-                    disputed
-                    cancelledDate
-                    committedDate
-                    completedDate
-                    disputedDate
-                    expired
-                    finalizedDate
-                    redeemedDate
-                    revokedDate
-                    validUntilDate
-                    state
-                  }
-                  metadata {
-                    ... on ProductV1MetadataEntity {
-                      name
-                      image
-                    }
-                  }
-                  disputeResolver {
-                    escalationResponsePeriod
-                  }
-                }
-              }
-            }
-          }
-        `,
+        buildQuery(
+          `escalatedDate_not: null, refusedDate_not: null`,
+          `getPastEscalatedDisputesWithDecisions`
+        ),
         props
       );
       return result;
