6181afca4488ec2790d1514bfc4167cc341357e4
Rafał Mikucki <rafalmikucki@gmail.com>
2022-09-30 08:03:10 +0200

    fix: remove lazyloading from some of the pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,12 +6,12 @@ import {
   OffersRoutes,
   SellerCenterRoutes
 } from "../lib/routing/routes";
+import CreateProductPage from "../pages/create-product/CreateProduct";
+import ExplorePage from "../pages/explore/Explore";
 import LandingPage from "../pages/landing/Landing";
+import SellerCenterPage from "../pages/sell/SellerCenter";
 
 const ChatPage = lazy(() => import("../pages/chat/Chat"));
-const CreateProductPage = lazy(
-  () => import("../pages/create-product/CreateProduct")
-);
 const CustomStorePage = lazy(() => import("../pages/custom-store/CustomStore"));
 const DisputeCentrePage = lazy(
   () => import("../pages/dispute-centre/DisputeCentre")
@@ -20,7 +20,6 @@ const DisputeListPage = lazy(
   () => import("../pages/dispute-centre/DisputeList")
 );
 const ExchangePage = lazy(() => import("../pages/exchange/Exchange"));
-const ExplorePage = lazy(() => import("../pages/explore/Explore"));
 const NotFoundPage = lazy(() => import("../pages/not-found/NotFound"));
 const OfferDetailPage = lazy(() => import("../pages/offers/OfferDetail"));
 const PrivateAccountPage = lazy(
@@ -30,7 +29,6 @@ const ProfilePagePage = lazy(() => import("../pages/profile/ProfilePage"));
 const PublicOrPrivateAccountPage = lazy(
   () => import("../pages/account/public/PublicOrPrivateAccount")
 );
-const SellerCenterPage = lazy(() => import("../pages/sell/SellerCenter"));
 
 export const baseAppProps = {
   withLayout: true,
