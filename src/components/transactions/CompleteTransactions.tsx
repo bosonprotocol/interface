77c6566e7e639564c5abbe9cf3768b6c16784bda
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-10-18 10:30:04 +0200

    refactor: properly use sdk methods and types for completed tx (#321)
    * refactor: properly use sdk methods and types for completed tx
    
    * refactor: use more suitable hooks

--- src/components/transactions/CompleteTransactions.tsx
+++ src/components/transactions/CompleteTransactions.tsx
@@ -1,167 +1,169 @@
+import { subgraph } from "@bosonprotocol/core-sdk";
 import { useMemo } from "react";
+import { generatePath } from "react-router-dom";
 
-import {
-  CompleteTransactionLogs,
-  useGetCompletedTxLogsByWallet
-} from "../../lib/utils/hooks/getTransactions/getTransactions";
+import { BosonRoutes, OffersRoutes } from "../../lib/routing/routes";
+import { useCompletedTransactions } from "../../lib/utils/hooks/transactions/useCompletedTransactions";
 import Grid from "../ui/Grid";
 import Loading from "../ui/Loading";
 import TransactionsTable from "./TransactionsTable";
-enum AccountType {
-  BUYER = "Buyer",
-  SELLER = "Seller",
-  DISPUTE_RESOLVER = "Dispute resolver"
-}
 
-enum OfferTransactions {
-  BUYER_COMMITTED = "BUYER_COMMITTED", // offer
-  OFFER_CREATED = "OFFER_CREATED", //offer
-  OFFER_VOIDED = "OFFER_VOIDED" // offer
-}
+const buildTransactionLabelAndPath = (
+  eventLog: subgraph.BaseEventLogFieldsFragment
+): { label: string; pathname?: string } => {
+  switch (eventLog.type) {
+    case subgraph.EventType.BuyerCommitted:
+      return {
+        label: `Committed to offer with id: ${
+          (eventLog as subgraph.ExchangeEventLog).exchange.offer.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.ExchangeEventLog).exchange.id
+        })
+      };
+    case subgraph.EventType.BuyerCreated:
+      return { label: "Buyer created" };
+    case subgraph.EventType.DisputeDecided:
+      return {
+        label: `Decided dispute with id: ${
+          (eventLog as subgraph.DisputeEventLog).dispute.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.DisputeEventLog).dispute.id
+        })
+      };
+    case subgraph.EventType.DisputeEscalated:
+      return {
+        label: `Escalated dispute with id: ${
+          (eventLog as subgraph.DisputeEventLog).dispute.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.DisputeEventLog).dispute.id
+        })
+      };
+    case subgraph.EventType.DisputeRaised:
+      return {
+        label: `Raised dispute with id: ${
+          (eventLog as subgraph.DisputeEventLog).dispute.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.DisputeEventLog).dispute.id
+        })
+      };
+    case subgraph.EventType.DisputeResolved:
+      return {
+        label: `Resolved dispute with id: ${
+          (eventLog as subgraph.DisputeEventLog).dispute.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.DisputeEventLog).dispute.id
+        })
+      };
+    case subgraph.EventType.EscalatedDisputeRefused:
+      return {
+        label: `Refused dispute with id: ${
+          (eventLog as subgraph.DisputeEventLog).dispute.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.DisputeEventLog).dispute.id
+        })
+      };
+    case subgraph.EventType.ExchangeCompleted:
+      return {
+        label: `Complete exchange with id: ${
+          (eventLog as subgraph.DisputeEventLog).dispute.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.DisputeEventLog).dispute.id
+        })
+      };
+    case subgraph.EventType.FundsDeposited:
+      return { label: `Funds deposited` };
+    case subgraph.EventType.FundsEncumbered:
+      return { label: `Funds encumbered` };
+    case subgraph.EventType.FundsReleased:
+      return { label: `Funds released` };
+    case subgraph.EventType.FundsWithdrawn:
+      return { label: `Funds withdrawn` };
+    case subgraph.EventType.OfferCreated:
+      return {
+        label: `Created offer with id: ${
+          (eventLog as subgraph.OfferEventLog).offer.id
+        }`,
+        pathname: generatePath(OffersRoutes.OfferDetail, {
+          id: (eventLog as subgraph.OfferEventLog).offer.id
+        })
+      };
+    case subgraph.EventType.OfferVoided:
+      return {
+        label: `Voided offer with id: ${
+          (eventLog as subgraph.OfferEventLog).offer.id
+        }`,
+        pathname: generatePath(OffersRoutes.OfferDetail, {
+          id: (eventLog as subgraph.OfferEventLog).offer.id
+        })
+      };
 
-export enum TransctionTypes {
-  BUYER_COMMITTED = "BUYER_COMMITTED", // offer
-  OFFER_CREATED = "OFFER_CREATED", //offer
-  OFFER_VOIDED = "OFFER_VOIDED", // offer
-  FUNDS_ENCUMBERED = "FUNDS_ENCUMBERED", // other
-  VOUCHER_REDEEMED = "VOUCHER_REDEEMED", // exchange
-  DISPUTE_RESOLVED = "DISPUTE_RESOLVED", // exchange
-  DISPUTE_DECIDED = "DISPUTE_DECIDED", // exchange
-  FUNDS_RELEASED = "FUNDS_RELEASED", // exchange
-  DISPUTE_RAISED = "DISPUTE_RAISED", // exchange
-  DISPUTE_ESCALATED = "DISPUTE_ESCALATED", // exchange
-  ESCALATED_DISPUTE_REFUSED = "ESCALATED_DISPUTE_REFUSED", // exchange
-  VOUCHER_REVOKED = "VOUCHER_REVOKED", // exchange
-  VOUCHER_CANCELED = "VOUCHER_CANCELED", // exchange
-  FUNDS_WITHDRAWN = "FUNDS_WITHDRAWN", // other
-  BUYER_CREATED = "BUYER_CREATED", // other
-  EXCHANGE_COMPLETED = "EXCHANGE_COMPLETED", // exchange
-  FUNDS_DEPOSITED = "FUNDS_DEPOSITED",
-  SELLER_CREATED = "SELLER_CREATED",
-  UPDATE_SELLER = "UPDATE_SELLER"
-}
+    case subgraph.EventType.VoucherCanceled:
+      return {
+        label: `Canceled offer with id: ${
+          (eventLog as subgraph.ExchangeEventLog).exchange.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.ExchangeEventLog).exchange.id
+        })
+      };
 
-const buildTransaction = (type: TransctionTypes): string => {
-  switch (type) {
-    case TransctionTypes.BUYER_COMMITTED:
-      return "Committed to offer";
-    case TransctionTypes.BUYER_CREATED:
-      return "Buyer created";
-    case TransctionTypes.DISPUTE_DECIDED:
-      return "Decided dispute";
-    case TransctionTypes.DISPUTE_ESCALATED:
-      return "Escalated dispute";
-    case TransctionTypes.DISPUTE_RAISED:
-      return "Raised dispute";
-    case TransctionTypes.DISPUTE_RESOLVED:
-      return "Resolved dispute";
-    case TransctionTypes.ESCALATED_DISPUTE_REFUSED:
-      return "Refused dispute";
-    case TransctionTypes.EXCHANGE_COMPLETED:
-      return "Complete exchange";
-    case TransctionTypes.FUNDS_DEPOSITED:
-      return "Funds deposited";
-    case TransctionTypes.FUNDS_ENCUMBERED:
-      return "Funds encumbered";
-    case TransctionTypes.FUNDS_RELEASED:
-      return "Funds released";
-    case TransctionTypes.FUNDS_WITHDRAWN:
-      return "Funds Withdrawn";
-    case TransctionTypes.OFFER_CREATED:
-      return "Created offer";
-    case TransctionTypes.OFFER_VOIDED:
-      return "Voided offer";
-    case TransctionTypes.VOUCHER_CANCELED:
-      return "Canceled voucher";
-    case TransctionTypes.VOUCHER_REDEEMED:
-      return "Redeemed voucher";
-    case TransctionTypes.VOUCHER_REVOKED:
-      return "Revoked voucher";
-    case TransctionTypes.SELLER_CREATED:
-      return "Seller created";
-    case TransctionTypes.UPDATE_SELLER:
-      return "Seller updated";
+    case subgraph.EventType.VoucherRedeemed:
+      return {
+        label: `Redeemed offer with id: ${
+          (eventLog as subgraph.ExchangeEventLog).exchange.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.ExchangeEventLog).exchange.id
+        })
+      };
+
+    case subgraph.EventType.VoucherRevoked:
+      return {
+        label: `Revoked voucher with id: ${
+          (eventLog as subgraph.ExchangeEventLog).exchange.id
+        }`,
+        pathname: generatePath(BosonRoutes.Exchange, {
+          id: (eventLog as subgraph.ExchangeEventLog).exchange.id
+        })
+      };
+    case subgraph.EventType.SellerUpdated:
+      return { label: `Seller updated` };
+    case subgraph.EventType.SellerCreated:
+      return { label: `Seller created` };
     default:
-      return `Not supported transaction ${type}`;
+      return { label: `Unknown transaction ${eventLog.type}` };
   }
 };
 
-const buildTableData = (data?: CompleteTransactionLogs) => {
-  const buyers =
-    data?.buyers[0]?.logs?.map((tx) => {
-      const splitId = tx.id.split("-");
-      const offerExchangeId = splitId[1];
-      const buyerSellerID = splitId[2];
-
-      return {
-        account: AccountType.BUYER,
-        transaction: buildTransaction(tx.type),
-        date: tx.timestamp,
-        executed: tx.executedBy,
-        hash: tx.hash,
-        offerExchangeId,
-        buyerSellerID,
-        isOffer: Object.values(OfferTransactions).includes(
-          tx.type as unknown as OfferTransactions
-        )
-      };
-    }) || [];
-
-  const sellers =
-    data?.sellers[0]?.logs?.map((tx) => {
-      const splitId = tx.id.split("-");
-      const offerExchangeId = splitId[1];
-      const buyerSellerID = splitId[2];
-      return {
-        account: AccountType.SELLER,
-        transaction: buildTransaction(tx.type),
-        date: tx.timestamp,
-        executed: tx.executedBy,
-        hash: tx.hash,
-        offerExchangeId,
-        buyerSellerID,
-        isOffer: Object.values(OfferTransactions).includes(
-          tx.type as unknown as OfferTransactions
-        )
-      };
-    }) || [];
-
-  const disputeResolvers =
-    data?.disputeResolvers[0]?.logs?.map((tx) => {
-      const splitId = tx.id.split("-");
-      const offerExchangeId = splitId[1];
-      const buyerSellerID = splitId[2];
-      return {
-        account: AccountType.DISPUTE_RESOLVER,
-        transaction: buildTransaction(tx.type),
-        date: tx.timestamp,
-        executed: tx.executedBy,
-        hash: tx.hash,
-        offerExchangeId,
-        buyerSellerID,
-        isOffer: Object.values(OfferTransactions).includes(
-          tx.type as unknown as OfferTransactions
-        )
-      };
-    }) || [];
-
-  /**
-   * TODO: this is not the best performante sort algorithm, good enought for the
-   * amount of transaction that we have for
-   * launch
-   */
-  const mixedList = [...buyers, ...sellers, ...disputeResolvers].sort(
-    (a, b) => {
-      return parseInt(b.date) - parseInt(a.date);
-    }
-  );
-
-  return mixedList;
+const buildTableData = (
+  eventLogs: Array<
+    subgraph.BaseEventLogFieldsFragment & { accountType: string }
+  >
+) => {
+  return eventLogs.map((log) => {
+    const { label, pathname } = buildTransactionLabelAndPath(log);
+    return {
+      accountType: log.accountType,
+      transactionLabel: label,
+      date: log.timestamp,
+      hash: log.hash,
+      pathname
+    };
+  });
 };
 
 export const CompletedTransactions = () => {
-  const { data } = useGetCompletedTxLogsByWallet();
-  const tableData = useMemo(() => (data ? buildTableData(data) : []), [data]);
+  // TODO: implement infinite scroll for table
+  const { data } = useCompletedTransactions();
+
+  const tableData = useMemo(() => buildTableData(data || []), [data]);
 
   if (data === undefined) {
     return <Loading />;
