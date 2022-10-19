506656e279a4120633f3276647d8fc3ae9dbbbf6
Rafał Mikucki <rafalmikucki@gmail.com>
2022-10-19 12:39:06 +0200

    fix: validation of file

--- src/components/product/utils/validationUtils.ts
+++ src/components/product/utils/validationUtils.ts
@@ -5,11 +5,16 @@ import { FileProps } from "../../form/Upload/WithUploadToIpfs";
 import { SUPPORTED_FILE_FORMATS } from "./const";
 
 export const validationOfRequiredIpfsImage = () =>
-  Yup.mixed<FileProps[]>().test(
-    "fileUploaded",
-    "You need to upload an image",
-    (value) => !value || (value && value.length !== 0)
-  );
+  Yup.mixed<FileProps[]>().test("fileUploaded", function (value) {
+    console.log("YUP value", value);
+    if (!value || (value && value.length !== 0)) {
+      throw this.createError({
+        path: this.path,
+        message: "You need to upload file!"
+      });
+    }
+    return true;
+  });
 
 export const validationOfIpfsImage = () => Yup.mixed<FileProps[]>();
 
