70128d33142cae58a09aa80a68ec75fa029b3728
Rafal Mikucki <rafalmikucki@gmail.com>
2022-10-21 18:53:21 +0200

    feat: boson to eth and eth to usdc (#341)
    * refactor: simplify queries
    
    * feat: boson to eth and eth to usdc
    
    * fix: uniswap eth rejected issue
    
    * fix: isDev change to proper value

--- src/components/convertion-rate/utils.ts
+++ src/components/convertion-rate/utils.ts
@@ -1,3 +1,4 @@
+import { Offer } from "../../lib/types/offer";
 import { IPool } from "../../lib/utils/hooks/useUniswapPools";
 
 export interface RateProps {
@@ -5,29 +6,54 @@ export interface RateProps {
   from: string;
   value: number;
 }
-export function handleRates(data: Array<IPool>): Array<RateProps | null> {
-  return data && data.length
-    ? data
-        ?.map((d: IPool) => {
-          const swapTokens = (d?.token0?.symbol || "")
-            .toLowerCase()
-            .startsWith("usd");
-          const token0 = swapTokens ? d?.token1?.symbol : d?.token0?.symbol;
-          const token1 = swapTokens ? d?.token0?.symbol : d?.token1?.symbol;
-          const value = swapTokens
-            ? Number(d?.token0Price)
-            : Number(d?.token1Price);
+interface RatePools {
+  all: Array<IPool>;
+  boson: Array<IPool>;
+}
+export function handleRates(
+  data: RatePools,
+  tokens: Pick<Offer["exchangeToken"], "address" | "symbol">[]
+): Array<RateProps> {
+  const allRates = (
+    data?.all?.map((d: IPool) => {
+      const swapTokens = (d?.token0?.symbol || "")
+        .toLowerCase()
+        .startsWith("usd");
+      const token0 = swapTokens ? d?.token1?.symbol : d?.token0?.symbol;
+      const token1 = swapTokens ? d?.token0?.symbol : d?.token1?.symbol;
+      const value = swapTokens
+        ? Number(d?.token0Price)
+        : Number(d?.token1Price);
 
-          if (value === 0) {
-            return null;
-          }
+      if (value === 0) {
+        return null;
+      }
 
-          return {
-            to: token0,
-            from: token1,
-            value
-          };
-        })
-        .filter((n) => n !== null)
-    : [];
+      return {
+        to: token0,
+        from: token1,
+        value
+      };
+    }) || []
+  ).filter((n) => n !== null) as RateProps[];
+
+  const bosonToEth =
+    data?.boson?.find((d: IPool) => d?.token0?.symbol === "WETH") || null;
+  const ethToUsd =
+    data?.boson?.find((d: IPool) => d?.token0?.symbol === "USDC") || null;
+
+  const bosonRate = {
+    to: "BOSON",
+    from: "USDC",
+    value:
+      Number(bosonToEth?.token0Price || 0) * Number(ethToUsd?.token0Price || 0)
+  };
+
+  const filteredRates = tokens?.map(
+    (t: Pick<Offer["exchangeToken"], "address" | "symbol">) => {
+      return allRates?.find((d: RateProps) => d?.to === t?.symbol) || null;
+    }
+  );
+
+  return [...filteredRates, bosonRate].filter((n) => n !== null) as RateProps[];
 }
