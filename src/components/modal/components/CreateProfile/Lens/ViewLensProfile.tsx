5e6f3307c3c48285548100c89f3be5339c0842b8
Dong-Ha Kim <dongha.kim210@gmail.com>
2022-11-07 19:33:30 +0700

    catch error

--- src/components/modal/components/CreateProfile/Lens/ViewLensProfile.tsx
+++ src/components/modal/components/CreateProfile/Lens/ViewLensProfile.tsx
@@ -73,52 +73,54 @@ export default function ViewLensProfile({
     Promise.all([
       fetchImageAsBase64(profilePictureUrl),
       fetchImageAsBase64(coverPictureUrl)
-    ]).then(([profilePictureBase64, coverPictureBase64]) => {
-      const profileType = profilePictureBase64
-        ? profilePictureBase64.split(";")[0].split(":")[1]
-        : "";
-      const profileFileType = profileType.includes("image")
-        ? profileType
-        : "image/jpg";
-      const coverType = coverPictureBase64
-        ? coverPictureBase64.split(";")[0].split(":")[1]
-        : "";
-      const coverFileType = coverType.includes("image")
-        ? coverType
-        : "image/jpg";
-      setValues({
-        logo: profilePictureBase64
-          ? [
-              new File(
-                [dataURItoBlob(profilePictureBase64)],
-                "profilePicture",
-                {
-                  type: profileFileType
-                }
-              )
-            ]
-          : [],
-        coverPicture: coverPictureBase64
-          ? [
-              new File([dataURItoBlob(coverPictureBase64)], "coverPicture", {
-                type: coverFileType
-              })
-            ]
-          : [],
-        name: profile.name || "",
-        handle:
-          profile.handle?.substring(
-            0,
-            profile.handle.lastIndexOf(CONFIG.lens.lensHandleExtension) < 0
-              ? profile.handle.lastIndexOf(CONFIG.lens.lensHandleExtension)
-              : profile.handle.lastIndexOf(".")
-          ) || "",
-        email: getLensEmail(profile) || "",
-        description: profile.bio || "",
-        website: getLensWebsite(profile) || "",
-        legalTradingName: getLensLegalTradingName(profile) || ""
-      });
-    });
+    ])
+      .then(([profilePictureBase64, coverPictureBase64]) => {
+        const profileType = profilePictureBase64
+          ? profilePictureBase64.split(";")[0].split(":")[1]
+          : "";
+        const profileFileType = profileType.includes("image")
+          ? profileType
+          : "image/jpg";
+        const coverType = coverPictureBase64
+          ? coverPictureBase64.split(";")[0].split(":")[1]
+          : "";
+        const coverFileType = coverType.includes("image")
+          ? coverType
+          : "image/jpg";
+        setValues({
+          logo: profilePictureBase64
+            ? [
+                new File(
+                  [dataURItoBlob(profilePictureBase64)],
+                  "profilePicture",
+                  {
+                    type: profileFileType
+                  }
+                )
+              ]
+            : [],
+          coverPicture: coverPictureBase64
+            ? [
+                new File([dataURItoBlob(coverPictureBase64)], "coverPicture", {
+                  type: coverFileType
+                })
+              ]
+            : [],
+          name: profile.name || "",
+          handle:
+            profile.handle?.substring(
+              0,
+              profile.handle.lastIndexOf(CONFIG.lens.lensHandleExtension) < 0
+                ? profile.handle.lastIndexOf(CONFIG.lens.lensHandleExtension)
+                : profile.handle.lastIndexOf(".")
+            ) || "",
+          email: getLensEmail(profile) || "",
+          description: profile.bio || "",
+          website: getLensWebsite(profile) || "",
+          legalTradingName: getLensLegalTradingName(profile) || ""
+        });
+      })
+      .catch((e) => console.error(e));
   }, [profile, profilePictureUrl, coverPictureUrl, setValues]);
 
   return (
