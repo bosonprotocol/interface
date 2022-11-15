58577dbcf6d060cffda75ccd84f6c21be52fbe24
Roberto Sousa <beto.sousa22@gmail.com>
2022-11-15 11:46:18 +0000

    chore: remove dev logs

--- src/pages/create-product/CreateProduct.tsx
+++ src/pages/create-product/CreateProduct.tsx
@@ -23,7 +23,6 @@ function CreateProduct() {
   };
 
   const showCreateProductDraftModal = useCallback(() => {
-    console.log("26-1 roberto");
     if (store.shouldDisplayModal) {
       showModal(modalTypes.CREATE_PRODUCT_DRAFT, {
         title: "Draft",
@@ -31,14 +30,12 @@ function CreateProduct() {
         chooseDraft
       });
     } else {
-      console.log("34-1 roberto");
       setDraftModalClosed(true);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const showInvalidRoleModal = useCallback(() => {
-    console.log("41-1 roberto");
     showModal<"INVALID_ROLE">(modalTypes.INVALID_ROLE, {
       title: "Invalid Role",
       action: "create a product",
@@ -48,8 +45,6 @@ function CreateProduct() {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  console.log("roberto 49-1");
 
   return (
     <CreateProductInner
