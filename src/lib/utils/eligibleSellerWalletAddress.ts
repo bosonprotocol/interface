67947c20b4123438f0248ff6dcc48a271ccc9b0c
Piotr Kniaź <piotr.kniaz@arianelabs.com>
2022-11-18 12:51:58 +0100

    feat: parseEligibleSellerWalletAddress update

--- src/lib/utils/eligibleSellerWalletAddress.ts
+++ src/lib/utils/eligibleSellerWalletAddress.ts
@@ -2,9 +2,7 @@ export function parseEligibleSellerWalletAddress(
   value?: string
 ): string[] | undefined {
   if (value) {
-    return value
-      .split(",")
-      .map((address) => address.replaceAll(" ", "").toLowerCase());
+    return value.split(",").map((address) => address.trim().toLowerCase());
   }
 
   return [];
