0f9dceb615b1c0e38ba239765671427249bc62a3
max-koz <maksymilian.kozak@arianelabs.com>
2022-11-25 15:14:38 +0100

    fix: wip

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -79,7 +79,7 @@ export const CONFIG = {
     ticker: process.env.DEFAULT_CURRENCY || "USD",
     symbol: process.env.DEFAULT_CURRENCY_SYMBOL || "$"
   },
-  releaseName: process.env.REACT_APP_RELEASE_NAME,
+  releaseName: process.env.REACT_APP_RELEASE_TAG,
   envName: REACT_APP_ENV_NAME as EnvironmentType,
   theGraphIpfsUrl:
     process.env.REACT_APP_THE_GRAPH_IPFS_URL || config.theGraphIpfsUrl,
