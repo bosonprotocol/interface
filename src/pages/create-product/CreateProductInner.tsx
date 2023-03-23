70815500feea947b8b3ccc30ee941f07e6bafbce
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2023-03-23 11:03:28 +0100

    fix: detail slider when images dont load and remove unused variable (#527)

--- src/pages/create-product/CreateProductInner.tsx
+++ src/pages/create-product/CreateProductInner.tsx
@@ -679,7 +679,6 @@ function CreateProductInner({
         const variantsForMetadataCreation: Parameters<
           typeof productV1["createVariantProductMetadata"]
         >[1] = [];
-        const variations: productV1.ProductV1Variant = [];
         const visualImages: productV1.ProductBase["visuals_images"] = [];
         const allVariationsWithSameImages =
           values.imagesSpecificOrAll?.value === "all";
@@ -702,7 +701,6 @@ function CreateProductInner({
               option: color || "-"
             }
           ];
-          variations.push(...typeOptions);
 
           if (!allVariationsWithSameImages && productImages) {
             const variantVisualImages = extractVisualImages(productImages);
