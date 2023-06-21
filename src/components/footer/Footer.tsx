cddca7ab539ee58dd43ca9551dcee6db5ef7d549
Albert Folch <albertfolch@redeemeum.com>
2023-06-21 16:47:53 +0200

    feat: add logo to detail widget and footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -272,8 +272,9 @@ function CustomStoreAdditionalLinks() {
 
 function ByBoson() {
   return (
-    <Grid justifyContent="center">
-      <Typography $fontSize="0.8rem">Powered by Boson</Typography>
+    <Grid justifyContent="center" alignItems="center" gap="0.5rem">
+      <Typography $fontSize="0.8rem">Powered by</Typography>
+      <img src={logo} alt="boson logo" width="100" />
     </Grid>
   );
 }
