274a938a31043849392bb18169e0f33b618a9480
albertfolch-redeemeum <102516373+albertfolch-redeemeum@users.noreply.github.com>
2023-06-21 13:10:34 +0200

    fix: crash in create product (#706)

--- src/components/product/ProductImages.tsx
+++ src/components/product/ProductImages.tsx
@@ -109,7 +109,7 @@ export default function ProductImages({ onChangeOneSetOfImages }: Props) {
     !hasVariants || values.imagesSpecificOrAll?.value === "all";
   const tabsData = useMemo(() => {
     return (
-      values.productVariants.variants?.map((variant, index) => {
+      values.productVariants?.variants?.map((variant, index) => {
         return {
           id: variant.name || index + "",
           title: variant.name || `Variant ${index}`,
