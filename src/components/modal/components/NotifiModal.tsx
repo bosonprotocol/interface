cb9a308b44a89cd1f918b74be93c4f6f66f692c3
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2023-06-22 16:41:32 +0100

    get the  topics to register too and the list of topics already regietered

--- src/components/modal/components/NotifiModal.tsx
+++ src/components/modal/components/NotifiModal.tsx
@@ -6,8 +6,12 @@ import {
   NotifiInputSeparators,
   NotifiSubscriptionCard
 } from "@notifi-network/notifi-react-card";
+import { useNotifiClient } from "@notifi-network/notifi-react-hooks";
 import { arrayify } from "ethers/lib/utils";
+import { useMemo, useState } from "react";
 import { useAccount, useSigner } from "wagmi";
+
+import { useChatContext } from "../../../pages/chat/ChatProvider/ChatContext";
 
 export default function NotifiModal() {
   const { data: signer } = useSigner();
@@ -40,9 +44,50 @@ export default function NotifiModal() {
     cardId: "f94c8ab903d849e2b70204c4357372cc"
   };
 
-  const xmtpInputs = {
-    XMTPTopics: ["xmtp-topic"]
-  };
+  const { bosonXmtp } = useChatContext();
+  const [areTopicsReady, setAreTopicsReady] = useState<boolean>(false);
+
+  const notifiClient = useNotifiClient({
+    dappAddress: notifiConfig.dappId,
+    env: "Development",
+    walletPublicKey: address as string,
+    walletBlockchain: "ETHEREUM"
+  });
+
+  const alreadyRegisteredTopics = useMemo(() => {
+    const sourceTopics = notifiClient.data?.alerts
+      ?.find(
+        (alert: { filter?: { filterType?: string } }) =>
+          alert.filter?.filterType === "WEB3_CHAT_MESSAGES"
+      )
+      ?.sourceGroup?.sources?.filter(
+        (source: unknown) => (source as { type?: string }).type === "XMTP"
+      )
+      ?.map(
+        (source: unknown) => (source as { name?: string }).name
+      ) as string[];
+    return sourceTopics || [];
+  }, [notifiClient]);
+
+  const topicDatas = useMemo(() => {
+    if (bosonXmtp) {
+      bosonXmtp.getConversations().then((convos) => {
+        const newTopicDatas = convos
+          .map((convo) => {
+            return { topic: convo.topic, peerAddress: convo.peerAddress };
+          })
+          .filter(
+            (topicData) => !alreadyRegisteredTopics.includes(topicData.topic)
+          );
+        setAreTopicsReady(true);
+        return newTopicDatas;
+      });
+    } else {
+      console.log("topics are not ready");
+      setAreTopicsReady(false);
+      return [];
+    }
+  }, [bosonXmtp, alreadyRegisteredTopics]);
 
   return (
     <>
@@ -61,13 +106,23 @@ export default function NotifiModal() {
           //If Binance, use "BINANCE"
           //If Optimism, use OPTIMISM
         >
-          <NotifiSubscriptionCard
-            cardId={notifiConfig.cardId}
-            inputLabels={inputLabels}
-            inputSeparators={inputSeparators}
-            darkMode //optional
-            inputs={xmtpInputs}
-          />
+          {areTopicsReady ? (
+            <NotifiSubscriptionCard
+              cardId={notifiConfig.cardId}
+              inputLabels={inputLabels}
+              inputSeparators={inputSeparators}
+              darkMode //optional
+              inputs={{
+                XMTPTopics:
+                  topicDatas?.map(
+                    (topicData: { topic: string; peerAddress: string }) =>
+                      topicData.topic
+                  ) || []
+              }}
+            />
+          ) : (
+            <p>Fetching XMTP topics...</p>
+          )}
         </NotifiContext>
       )}
     </>
