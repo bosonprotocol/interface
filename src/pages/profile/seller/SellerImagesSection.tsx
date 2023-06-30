c68806786c7aff5a8cbdd1c4f7a2d3b8e42beb41
Albert Folch <albertfolch@redeemeum.com>
2023-06-30 17:11:18 +0200

    wip

--- src/pages/profile/seller/SellerImagesSection.tsx
+++ src/pages/profile/seller/SellerImagesSection.tsx
@@ -9,18 +9,22 @@ import Grid from "../../../components/ui/Grid";
 import Image from "../../../components/ui/Image";
 import { breakpoint } from "../../../lib/styles/breakpoint";
 import { useBreakpoints } from "../../../lib/utils/hooks/useBreakpoints";
-import {
-  AvatarContainer,
-  // BannerImageLayer,
-  ProfileSectionWrapper
-} from "../ProfilePage.styles";
+import { AvatarContainer, ProfileSectionWrapper } from "../ProfilePage.styles";
 
-// const StyledBannerImage = styled(BannerImage)`
-//   max-width: 100%;
-// `;
-const hiddenHeight = "100px";
 const smallHeight = "9rem";
 const bigHeight = "11.875rem";
+
+const ButtonsContainer = styled.div`
+  width: 100%;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+  ${breakpoint.s} {
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+  box-sizing: border-box;
+`;
+
 const BannerImageLayer = styled.div`
   position: absolute;
   top: 0;
@@ -31,39 +35,19 @@ const BannerImageLayer = styled.div`
 `;
 
 const WrapperWrapper = styled.div.attrs({ "data-wrapper-wrapper": true })`
+  width: 100%;
   border: 1px solid green;
   display: flex;
   overflow: hidden;
   align-items: center;
   position: relative;
-  height: calc(${smallHeight} + ${hiddenHeight} * 2);
+  height: ${smallHeight};
   ${breakpoint.s} {
-    height: calc(${bigHeight} + ${hiddenHeight} * 2);
-  }
-  + * {
-    top: ${hiddenHeight};
+    height: ${bigHeight};
   }
   img {
     flex: 1;
   }
-`;
-
-const BannerWrapper = styled.div.attrs({ "data-banner-wrapper": true })<{
-  $isTop: boolean;
-}>`
-  border: 1px solid red;
-  pointer-events: none;
-  overflow: hidden;
-  /* background: blue; */
-  z-index: 1;
-  position: absolute;
-  ${({ $isTop }) => {
-    return css`
-      ${$isTop ? "top:0" : "bottom:0"};
-    `;
-  }}
-  width: 100%;
-  height: ${hiddenHeight};
 `;
 
 const StyledBannerImage = styled.img<{ $isObjectFitContain: boolean }>`
@@ -115,83 +99,91 @@ const SellerImagesSection: React.FC<SellerImagesSectionProps> = ({
   draggable
 }) => {
   const { isLteXS } = useBreakpoints();
-  const [resetCounter, setResetCounter] = useState<number>(0);
+  const [defaultPosition, setDefaultPosition] = useState<number>(0);
+  const handleReset = () => {
+    setDefaultPosition((prev) => prev + 1);
+  };
   const [isObjectFitContain, setObjectFitContain] = useState<boolean>(true);
   const imageRef = useRef<HTMLImageElement>(null);
   return (
-    <ProfileSectionWrapper>
-      <Grid marginBottom="1rem">
-        <Button
-          type="button"
-          onClick={() => {
-            setObjectFitContain((prev) => !prev);
-            if (imageRef.current) {
-              imageRef.current.style.objectPosition = "initial";
-            }
-            setResetCounter((prev) => ++prev);
-          }}
-        >
-          object-fit: {isObjectFitContain ? "contain" : "cover"}
-        </Button>
-        <Button
-          type="button"
-          onClick={() => {
-            if (imageRef.current) {
-              imageRef.current.style.objectPosition = "initial";
-            }
-            setResetCounter((prev) => ++prev);
-          }}
-        >
-          Reset
-        </Button>
-      </Grid>
-      <WrapperWrapper>
-        <BannerWrapper $isTop={true} />
-        <Draggable
-          // bounds="parent"
-          key={resetCounter}
-          disabled={!draggable}
-          onDrag={(e, data) => {
-            console.log("e", e, "data", data);
-            data.node.style.objectPosition = `${data.x}px ${data.y}px`;
-            if (!isObjectFitContain) {
-              // data.node.style.clipPath = `polygon(0 ${data.y}px, 100% ${data.y}px, 100% 63%, 0 63%)`;
-            }
-          }}
-        >
-          <StyledBannerImage
-            ref={imageRef}
-            src={
-              "https://ik.imagekit.io/lens/media-snapshot/b7c1682e55814fec77bedc631f7713a64ba56b3d05c0ed3c3078bfacae519750.webp?img-format=auto" ||
-              coverImage ||
-              whiteImg
-            }
-            data-cover-img
-            draggable={false}
-            $isObjectFitContain={!!isObjectFitContain}
-          />
-        </Draggable>
-        <BannerWrapper $isTop={false} />
-      </WrapperWrapper>
-      <BannerImageLayer>
-        <AvatarContainer>
-          {profileImage ? (
-            <StyledImage
-              src={profileImage}
-              style={{
-                width: "160px !important",
-                height: "160px !important",
-                paddingTop: "0",
-                borderRadius: "50%",
-                backgroundColor: "var(--primaryBgColor)"
-              }}
+    <Grid flexDirection="column">
+      <ButtonsContainer>
+        <Grid marginBottom="1rem" justifyContent="space-between" gap="1rem">
+          <Button
+            type="button"
+            onClick={() => {
+              setObjectFitContain((prev) => !prev);
+              if (imageRef.current) {
+                imageRef.current.style.objectPosition = "center";
+              }
+              handleReset();
+            }}
+          >
+            object-fit: {isObjectFitContain ? "contain" : "cover"}
+          </Button>
+          <Button
+            type="button"
+            onClick={() => {
+              if (imageRef.current) {
+                imageRef.current.style.objectPosition = "center";
+              }
+              handleReset();
+            }}
+          >
+            Reset
+          </Button>
+        </Grid>
+      </ButtonsContainer>
+      <ProfileSectionWrapper>
+        <WrapperWrapper>
+          <Draggable
+            // bounds="parent"
+            // key={"" + defaultPosition}
+            disabled={!draggable}
+            onDrag={(e, data) => {
+              console.log("e", e, "data", data);
+              data.node.style.objectPosition = `${data.x}px ${data.y}px`;
+              if (!isObjectFitContain) {
+                // data.node.style.clipPath = `polygon(0 ${data.y}px, 100% ${data.y}px, 100% 63%, 0 63%)`;
+              }
+            }}
+          >
+            <StyledBannerImage
+              ref={imageRef}
+              src={
+                "https://ik.imagekit.io/lens/media-snapshot/b7c1682e55814fec77bedc631f7713a64ba56b3d05c0ed3c3078bfacae519750.webp?img-format=auto" ||
+                coverImage ||
+                whiteImg
+              }
+              data-cover-img
+              draggable={false}
+              $isObjectFitContain={!!isObjectFitContain}
             />
-          ) : (
-            <Avatar address={currentSellerAddress} size={!isLteXS ? 160 : 80} />
-          )}
-        </AvatarContainer>
-      </BannerImageLayer>
-    </ProfileSectionWrapper>
+          </Draggable>
+        </WrapperWrapper>
+        <BannerImageLayer>
+          <AvatarContainer>
+            {profileImage ? (
+              <StyledImage
+                src={profileImage}
+                style={{
+                  width: "160px !important",
+                  height: "160px !important",
+                  paddingTop: "0",
+                  borderRadius: "50%",
+                  backgroundColor: "var(--primaryBgColor)"
+                }}
+              />
+            ) : (
+              <Avatar
+                address={currentSellerAddress}
+                size={!isLteXS ? 160 : 80}
+              />
+            )}
+          </AvatarContainer>
+        </BannerImageLayer>
+      </ProfileSectionWrapper>
+    </Grid>
   );
 };
 
