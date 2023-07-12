e202ccd26f64af8ee9de58907efef87d662db214
levalleux-ludo <levalleux_ludo@hotmail.com>
2023-07-12 16:06:23 +0100

    optimize hook useCheckExchangePolicy

--- src/lib/utils/hooks/offer/useCheckExchangePolicy.ts
+++ src/lib/utils/hooks/offer/useCheckExchangePolicy.ts
@@ -10,50 +10,70 @@ interface Props {
   offerId: string | undefined;
 }
 
-export default function useCheckExchangePolicy({
-  offerId
-}: Props): offers.CheckExchangePolicyResult | undefined {
+const getRulesTemplate = async (
+  fairExchangePolicyRules: string | undefined
+) => {
+  if (!fairExchangePolicyRules) {
+    return undefined;
+  }
+  const rulesTemplateUri = getIpfsGatewayUrl(fairExchangePolicyRules as string);
+  const rulesTemplateText = await fetchTextFile(rulesTemplateUri);
+  const rulesTemplate = JSON.parse(
+    rulesTemplateText
+  ) as offers.CheckExchangePolicyRules;
+  // replace DEFAULT_DISPUTE_RESOLVER_ID (environment dependent)
+  const disputeResolverId_matches =
+    rulesTemplate?.yupSchema?.properties?.disputeResolverId?.matches?.replace(
+      "<DEFAULT_DISPUTE_RESOLVER_ID>",
+      CONFIG.defaultDisputeResolverId
+    );
+  if (disputeResolverId_matches) {
+    rulesTemplate.yupSchema.properties.disputeResolverId.matches =
+      disputeResolverId_matches;
+  }
+  // replace TOKENS_LIST (environment dependent)
+  const tokensList = CONFIG.defaultTokens.map((token) => token.address);
+  const tokensList_pattern =
+    rulesTemplate?.yupSchema?.properties?.exchangeToken?.properties?.address?.pattern?.replace(
+      "<TOKENS_LIST>",
+      tokensList.join("|")
+    );
+  if (
+    rulesTemplate.yupSchema.properties.exchangeToken.properties &&
+    tokensList_pattern
+  ) {
+    rulesTemplate.yupSchema.properties.exchangeToken.properties.address.pattern =
+      tokensList_pattern;
+  }
+  return rulesTemplate;
+};
+
+export default function useCheckExchangePolicy({ offerId }: Props) {
   const [result, setResult] = useState<
     offers.CheckExchangePolicyResult | undefined
   >(undefined);
+  const [rulesTemplate, setRulesTemplate] = useState<
+    offers.CheckExchangePolicyRules | undefined
+  >(undefined);
   const core = useCoreSDK();
   useEffect(() => {
-    if (!core || !offerId || !CONFIG.fairExchangePolicyRules) {
+    (async () =>
+      setRulesTemplate(
+        await getRulesTemplate(CONFIG.fairExchangePolicyRules)
+      ))();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [CONFIG.fairExchangePolicyRules]);
+  useEffect(() => {
+    if (
+      !core ||
+      !offerId ||
+      !CONFIG.fairExchangePolicyRules ||
+      !rulesTemplate
+    ) {
       return undefined;
     }
     (async () => {
       try {
-        const rulesTemplateUri = getIpfsGatewayUrl(
-          CONFIG.fairExchangePolicyRules as string
-        );
-        const rulesTemplateText = await fetchTextFile(rulesTemplateUri);
-        const rulesTemplate = JSON.parse(
-          rulesTemplateText
-        ) as offers.CheckExchangePolicyRules;
-        // replace DEFAULT_DISPUTE_RESOLVER_ID (environment dependent)
-        const disputeResolverId_matches =
-          rulesTemplate?.yupSchema?.properties?.disputeResolverId?.matches?.replace(
-            "<DEFAULT_DISPUTE_RESOLVER_ID>",
-            CONFIG.defaultDisputeResolverId
-          );
-        if (disputeResolverId_matches) {
-          rulesTemplate.yupSchema.properties.disputeResolverId.matches =
-            disputeResolverId_matches;
-        }
-        // replace TOKENS_LIST (environment dependent)
-        const tokensList = CONFIG.defaultTokens.map((token) => token.address);
-        const tokensList_pattern =
-          rulesTemplate?.yupSchema?.properties?.exchangeToken?.properties?.address?.pattern?.replace(
-            "<TOKENS_LIST>",
-            tokensList.join("|")
-          );
-        if (
-          rulesTemplate.yupSchema.properties.exchangeToken.properties &&
-          tokensList_pattern
-        ) {
-          rulesTemplate.yupSchema.properties.exchangeToken.properties.address.pattern =
-            tokensList_pattern;
-        }
         const _result = await core.checkExchangePolicy(offerId, rulesTemplate);
         setResult(_result);
       } catch (e) {
@@ -61,6 +81,6 @@ export default function useCheckExchangePolicy({
         setResult(undefined);
       }
     })();
-  }, [core, offerId]);
+  }, [core, offerId, rulesTemplate]);
   return result;
 }
