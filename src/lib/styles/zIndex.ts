3fe1d854a3eb3b1538c6afc734d252731653037d
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2023-08-16 16:19:01 +0100

    restore dApp-specific zIndex object

--- src/lib/styles/zIndex.ts
+++ src/lib/styles/zIndex.ts
@@ -1,3 +1,18 @@
-import { zIndex } from "@bosonprotocol/react-kit";
-
-export { zIndex };
+export const zIndex = {
+  Default: 0,
+  CommitStep: 0,
+  Button: 1,
+  OfferCard: 2,
+  ExchangeSidePreview: 2,
+  OfferStatus: 5,
+  ChatSeparator: 9,
+  Carousel: 10,
+  LandingTitle: 20,
+  Calendar: 30,
+  Select: 40,
+  Header: 1000,
+  Notification: 1001,
+  Modal: 2000,
+  Popper: 2001,
+  Cookie: 2001
+};
