eaed5a30a9f83a413d72f5ea6a2ce0da4a74b4ef
Albert Folch <albertfolch@redeemeum.com>
2023-08-28 12:30:25 +0200

    fix: button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,2 +1,22 @@
-import { ThemedButton } from "@bosonprotocol/react-kit";
-export default ThemedButton;
+import { IButton, ThemedButton } from "@bosonprotocol/react-kit";
+import { forwardRef } from "react";
+import styled, { css } from "styled-components";
+
+const StyledThemedButton = styled(ThemedButton)`
+  ${({ size }) => {
+    if (!size) {
+      return css`
+        height: auto;
+      `;
+    }
+    return css``;
+  }}
+`;
+
+export type ButtonProps = IButton;
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
+  return <StyledThemedButton {...props} ref={ref} />;
+});
+
+export default Button;
