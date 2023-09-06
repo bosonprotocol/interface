702b33f73744bd7fef9283e6587d17cfb6e5b42e
Albert Folch <albertfolch@redeemeum.com>
2023-09-06 12:05:05 +0200

    refactor: delete unused code

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,3 @@
-import { colors } from "lib/styles/colors";
 import styled from "styled-components";
 
 const Card = styled.div<{
@@ -14,35 +13,10 @@ const Card = styled.div<{
 `;
 export default Card;
 
-export const LightCard = styled(Card)`
-  border: 1px solid ${colors.lightGrey};
-  background-color: ${colors.darkGrey};
-`;
-
 export const GrayCard = styled(Card)`
   background-color: ${({ theme }) => theme.deprecated_bg3};
 `;
 
-export const DarkGrayCard = styled(Card)`
-  background-color: ${colors.lightGrey};
-`;
-
-export const DarkCard = styled(Card)`
-  background-color: ${colors.lightGrey};
-`;
-
 export const OutlineCard = styled(Card)`
   border: 1px solid ${({ theme }) => theme.deprecated_bg3};
-`;
-
-export const YellowCard = styled(Card)`
-  background-color: rgba(243, 132, 30, 0.05);
-  color: ${({ theme }) => theme.deprecated_yellow3};
-  font-weight: 500;
-`;
-
-export const BlueCard = styled(Card)`
-  background-color: ${({ theme }) => theme.deprecated_primary5};
-  color: ${({ theme }) => colors.secondary};
-  border-radius: 12px;
 `;
