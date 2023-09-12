25dd6b24200b1c55b4034aa6771e8ab388bb08fa
Albert Folch <albertfolch@redeemeum.com>
2023-09-12 17:49:58 +0200

    fix: wagmi connectio

--- src/components/WalletConnectionProvider.tsx
+++ src/components/WalletConnectionProvider.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable unused-imports/no-unused-imports */
 import { useWeb3React } from "@web3-react/core";
+import { MetaMask } from "@web3-react/metamask";
 import { ReactNode, useEffect } from "react";
-import { useAccount, useConnect, WagmiConfig } from "wagmi";
+import { useAccount, useConnect, useNetwork, WagmiConfig } from "wagmi";
 
 import { getConnectors, useWagmiConfig } from "../lib/wallet-connection";
 import { useConfigContext } from "./config/ConfigContext";
@@ -16,19 +18,39 @@ interface Props {
  */
 function ForceConnectWagmi({ children }: Props) {
   const { config } = useConfigContext();
-  const { account } = useWeb3React();
+  const { account, connector } = useWeb3React();
   const { address } = useAccount();
   const { connect } = useConnect();
+  const { chain, chains } = useNetwork();
   useEffect(() => {
-    if (account && !address && config.envConfig.chainId) {
+    if (
+      account &&
+      !address &&
+      config.envConfig.chainId &&
+      chains[0].id === config.envConfig.chainId && // we want to connect (enter if) if config.envConfig.chainId doesnt change anymore (at the start)
+      chain?.id !== config.envConfig.chainId
+    ) {
       const { connectors } = getConnectors(config.envConfig.chainId);
       const connectorsList = connectors();
       connect({
         chainId: config.envConfig.chainId,
-        connector: connectorsList[0] // TODO: verify this
+        connector:
+          connectorsList.find((connector_) =>
+            connector instanceof MetaMask
+              ? connector_.id === "metaMask"
+              : connector_.id === "walletConnect"
+          ) || connectorsList[0]
       });
     }
-  }, [account, address, config.envConfig.chainId, connect]);
+  }, [
+    account,
+    address,
+    config.envConfig.chainId,
+    connect,
+    connector,
+    chain?.id,
+    chains
+  ]);
   return <>{children}</>;
 }
 
