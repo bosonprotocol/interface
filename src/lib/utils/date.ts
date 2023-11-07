06c14391d2f3001788ebb12a9706e88848313127
Albert Folch <albertfolch@redeemeum.com>
2023-11-07 13:11:28 +0100

    fix: escalation modal

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -14,11 +14,20 @@ export const formatDate = (
     format?: string;
   } = {}
 ) => {
-  if (textIfTooBig && timestamp > MAX_AND_MIN_DATE_TIMESTAMP) {
+  if (textIfTooBig && checkIfTimestampIsToo("too_big", timestamp)) {
     return textIfTooBig;
   }
-  if (textIfTooSmall && timestamp < MAX_AND_MIN_DATE_TIMESTAMP) {
+  if (textIfTooSmall && checkIfTimestampIsToo("too_small", timestamp)) {
     return textIfTooSmall;
   }
   return dayjs(timestamp).format(format);
+};
+
+export const checkIfTimestampIsToo = (
+  checkIf: "too_big" | "too_small",
+  timestamp: number
+) => {
+  return checkIf === "too_big"
+    ? timestamp > MAX_AND_MIN_DATE_TIMESTAMP
+    : timestamp < MAX_AND_MIN_DATE_TIMESTAMP;
 };
