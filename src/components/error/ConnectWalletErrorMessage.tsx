b0c7f9cb913f7715f81d04993c1b1454215ace4e
Albert Folch <albertfolch@redeemeum.com>
2023-11-30 13:10:08 +0100

    fix: alt text in error pages

--- src/components/error/ConnectWalletErrorMessage.tsx
+++ src/components/error/ConnectWalletErrorMessage.tsx
@@ -16,7 +16,7 @@ export function ConnectWalletErrorMessage({
       cta={cta ?? <ConnectWallet />}
       message={message ?? "Please connect wallet to proceed"}
       title={title ?? "Connect wallet"}
-      img={<img src={frame} alt={`${title} image`} />}
+      img={<img src={frame} alt={title} />}
     />
   );
 }
