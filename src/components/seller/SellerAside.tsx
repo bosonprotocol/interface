6a9303d43bf9de11e012a675ec1e0145e88d883a
Albert Folch <albertfolch@redeemeum.com>
2023-12-14 16:59:47 +0100

    feat: add collapsible panel

--- src/components/seller/SellerAside.tsx
+++ src/components/seller/SellerAside.tsx
@@ -1,5 +1,6 @@
-import { WarningCircle } from "phosphor-react";
-import { useCallback } from "react";
+import { useBreakpoints } from "lib/utils/hooks/useBreakpoints";
+import { ArrowLineLeft, ArrowLineRight, WarningCircle } from "phosphor-react";
+import { ReactNode, useCallback, useState } from "react";
 import { generatePath, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -12,10 +13,36 @@ import { WithSellerDataProps } from "./common/WithSellerData";
 import { SellerInsideProps } from "./SellerInside";
 import { sellerPageTypes } from "./SellerPages";
 
+const ArrowContainer = styled.div`
+  display: grid;
+  align-items: center;
+  margin: 0.5rem 0.25rem;
+  cursor: pointer;
+  padding: 0.5rem;
+  border-radius: 9999px;
+  aspect-ratio: 1;
+  box-sizing: content-box;
+  width: 16px;
+  height: 16px;
+  :hover {
+    background-color: ${colors.lightGrey};
+  }
+`;
+
 const Aside = styled.aside`
+  display: flex;
+  align-items: flex-end;
+  flex-direction: column;
   background: ${colors.white};
+  .label {
+    transition: all 200ms;
+    width: 7.5em;
+  }
+  &.collapsed .label {
+    opacity: 0;
+    width: 0;
+  }
   ul {
-    width: 14.375rem;
     list-style: none;
     padding: 0;
     margin: 0;
@@ -57,6 +84,8 @@ export default function SellerAside(
   props: SellerInsideProps & WithSellerDataProps
 ) {
   const { [UrlParameters.sellerPage]: sellerPage } = useParams();
+  const { isLteXS } = useBreakpoints();
+  const [collapsed, setCollapsed] = useState(isLteXS);
 
   const handleUrl = useCallback((path: string, externalPath: string | null) => {
     if (externalPath !== null) {
@@ -67,9 +96,41 @@ export default function SellerAside(
       [UrlParameters.sellerPage]: path
     });
   }, []);
-
+  const IconContainer = useCallback(
+    ({ children }: { children: ReactNode }) => {
+      return collapsed ? (
+        <Grid
+          flexDirection="column"
+          alignItems="center"
+          flexGrow="0"
+          flexShrink="1"
+          flexBasis="0"
+        >
+          {children}
+        </Grid>
+      ) : (
+        <>{children}</>
+      );
+    },
+    [collapsed]
+  );
   return (
-    <Aside>
+    <Aside className={collapsed ? "collapsed" : ""}>
+      <ArrowContainer>
+        {collapsed ? (
+          <ArrowLineRight
+            weight="light"
+            size={16}
+            onClick={() => setCollapsed(false)}
+          />
+        ) : (
+          <ArrowLineLeft
+            weight="light"
+            size={16}
+            onClick={() => setCollapsed(true)}
+          />
+        )}
+      </ArrowContainer>
       <ul>
         {Object.keys(sellerPageTypes).map((key: string) => {
           const {
@@ -79,7 +140,9 @@ export default function SellerAside(
             icon: Icon
           } = sellerPageTypes[key as keyof typeof sellerPageTypes];
           const isActive = sellerPage === url;
-
+          props.offersBacked.displayWarning = true;
+          const showWarning =
+            label === "Finances" && props.offersBacked.displayWarning;
           return (
             <AsideLink key={`seller_aside_route_${label}`} $active={isActive}>
               <LinkWithQuery to={handleUrl(url, externalPath)}>
@@ -88,16 +151,24 @@ export default function SellerAside(
                   justifyContent="flex-start"
                   gap="1rem"
                 >
-                  <Icon
-                    size={16}
-                    weight={isActive ? "regular" : "thin"}
-                    color={isActive ? colors.black : colors.darkGrey}
-                  />
-                  {label}
-                  {label === "Finances" &&
-                    props.offersBacked.displayWarning && (
+                  <IconContainer>
+                    <Icon
+                      size={16}
+                      weight={isActive ? "regular" : "thin"}
+                      color={isActive ? colors.black : colors.darkGrey}
+                    />
+                    {collapsed && showWarning && (
                       <WarningCircle size={16} color={colors.orange} />
                     )}
+                  </IconContainer>
+                  <span className="label">{label}</span>
+                  {!collapsed && showWarning && (
+                    <WarningCircle
+                      size={16}
+                      color={colors.orange}
+                      style={{ marginRight: "0.5rem" }}
+                    />
+                  )}
                 </Grid>
               </LinkWithQuery>
             </AsideLink>
