f18fc3cb9f28ad0966fac44afffb63177e617a2a
Albert Folch <albertfolch@redeemeum.com>
2024-03-18 17:23:18 +0100

    clean code

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -6,19 +6,3 @@ export const useAppDispatch = () => useDispatch<typeof store.dispatch>();
 export const useAppSelector: TypedUseSelectorHook<
   ReturnType<typeof store.getState>
 > = useSelector;
-
-// import {
-//   createDispatchHook,
-//   createSelectorHook,
-//   TypedUseSelectorHook
-// } from "react-redux";
-// import { ReduxDappDummyContext } from "reduxContext";
-
-// import store from "./index";
-
-// const useDappDispatch = createDispatchHook(ReduxDappDummyContext as any);
-// export const useAppDispatch = () => useDappDispatch<typeof store.dispatch>();
-// const useDappSelector = createSelectorHook(ReduxDappDummyContext as any);
-// export const useAppSelector: TypedUseSelectorHook<
-//   ReturnType<typeof store.getState>
-// > = useDappSelector;
